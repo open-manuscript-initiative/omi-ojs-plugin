96553e92363c7526784f6a3062de25ea289094be
Varga István <vargawebkiado@gmail.com>
2026-09-15 10:00:39 +0200

    Implement OJS reviewer recommendation options

--- StudioIntegrationPlugin.php
+++ StudioIntegrationPlugin.php
@@ -370,8 +370,9 @@ class StudioIntegrationPlugin extends GenericPlugin
             'submission' => ['externalId' => (string)$submissionId],
             'reviewAssignment' => [
                 'externalId' => (string)$reviewAssignment->getId(),
+                'round' => (int)$reviewAssignment->getRound(),
             ],
-            'actor' => ['externalId' => (string)$user->getId()],
+              'actor' => ['externalId' => (string)$user->getId()],
             'actorMode' => 'review',
             'scope' => [
                 'review.metadata.read',
